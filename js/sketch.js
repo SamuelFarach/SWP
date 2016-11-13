185a0f3af81ad6403642673210a30b06e43423d2
Samuel Farach <farachsamuel@gmail.com>
2016-11-13 14:03:57 +0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -34,7 +34,7 @@ var xfighterColor = '#0000FF';//borrar;
 /****************************************************************************************************/
 function preload()
 {
-  deathstarCheip = loadImage('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
+  deathstarCheip = createImage('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
   //xfighterCheip = loadImage('SW_elemento_xWingFIghter.svg');
 }
 /****************************************************************************************************/
