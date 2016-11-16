ded80597d7103a8e823fb92ce03b60581c010ea0
Samuel Farach <farachsamuel@gmail.com>
2016-11-16 15:27:51 +0400

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -6,12 +6,14 @@ function preload() {
 
 function setup() {
     createCanvas(600, 200, SVG);
-    image(svg, 0, 0, 200, 200);
+    
     path = querySVG('path')[1];
 }
 
 function draw() {
     // update line width of 2nd line
+    background(0);
+    image(svg, 0, 0, 200, 200);
     path.attribute('stroke-width', frameCount % 20);
     if (frameCount === 18) {
         noLoop();
